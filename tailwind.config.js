1eae7581506c00bab66adea99439d47520318d6c
Dylan <d.bullock1404@outlook.com>
2024-10-09 10:47:40 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: '#367096',
+        medBlue: '#4887b7',
+        lightBlue: '#64adc4',
+        mint: '#8fdbc5',
+        lightMint: '#DBFFD3',
+      }
+    },
+  },
+  plugins: [],
+}
+
